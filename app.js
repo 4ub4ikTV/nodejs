a1661c9142359abf5643b434043fa9f0e0f22298
4ub4ik <svyantkoorest@gmail.com>
2023-05-30 13:41:59 +0300

    create less1

--- app.js
+++ app.js
@@ -9,4 +9,54 @@
 // !руками нічого не робимо, все через fs
 
 
+const fs = require('fs');
+
+const path = require('path');
+
+const fatherFolder = 'myFolder'
+
+const foldersName = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5']
+const filesName = ['text1.txt', 'text2.txt', 'text3.txt', 'text4.txt', 'text5.txt']
+
+fs.mkdir(path.join(__dirname, fatherFolder), (err) => {
+    if (err) console.error(`Сталась помилка при створенні папки ${fatherFolder}: ${err}`)
+    else
+        console.log(`Папку ${fatherFolder} успішно створено.`);
+})
+
+foldersName.forEach((folderName) => {
+    const foldersNamePath = path.join(fatherFolder, folderName);
+
+    fs.mkdir((foldersNamePath), (err) => {
+        if (err) {
+            console.error(`Сталась помилка при створенні папки ${foldersNamePath}: ${err}`);
+        } else {
+            console.log(`Папку ${foldersNamePath} успішно створено.`);
+        }
+    })
+})
+
+filesName.forEach((fileName) => {
+    const filesNamePath = path.join(fatherFolder, fileName)
+
+    fs.writeFile(filesNamePath, '', (err) => {
+        if (err) {
+            console.error(`Сталась помилка при створенні файлу ${filesNamePath}: ${err}`);
+        } else {
+            console.log(`Файл ${filesNamePath} успішно створено.`);
+        }
+    })
+})
+
+
+fs.readdir(path.join(fatherFolder), {withFileTypes: true}, (err, files) => {
+    if (err) throw new Error(err.message);
+    files.forEach(file => {
+        if (file.isDirectory()) {
+            console.log(`FOLDER`);
+        } else if (file.isFile()) {
+            console.log(`FILE`);
+        }
+    })
+})
 
